8c68bd425a72266f3f4acfc84b6d611808a01326
unknown <ejboland97@gmail.com>
2024-01-21 14:01:17 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'superSecret',
+  cookie: { maxAge: 600000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+console.log(typeof exphbs);
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
